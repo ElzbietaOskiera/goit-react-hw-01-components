b358febb994642151fa91a769631cfec846e34f7
ElzbietaOskiera <elaoskiera96@gmail.com>
2023-01-26 20:14:19 +0100

    bez stylowania

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,17 @@
+
+
+import { FriendListItem } from "components/FriendListItem/FriendListItem";
+
+export const FriendList = ({ friends }) => {
+    return (<ul class="friend-list">
+        {friends.map(friend => (
+            <FriendListItem
+                key={friend.id}
+                avatar={friend.avatar}
+                name={friend.name}
+                isOnline={friend.isOnline}
+            />
+        ))}
+    </ul>
+    );
+};
